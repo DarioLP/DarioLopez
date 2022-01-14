7cbc1b6356d628956867035bc4d431ade348eca5
DarioLP <lopezdario43@gmail.com>
2022-01-14 00:32:05 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Darío López</title>
+
+    <ul class="menu">
+        <label>DARÍO ANTONIO LÓPEZ POVEDA</label>
+			<li><a href="#">Inicio</a></li>
+			<li><a href="#">Proyectos</a></li>
+			<li><a href="#">Hoja de vida</a></li>
+			<li><a href="#">Contacto</a></li>
+		</ul>
+
+</head>
+<body>
+
+<div>
+    <p>
+        Hola soy Darío López, desarrollador de software en formación.<br>
+        También soy creador de Adrem Indie Lab, un mini laboratorio dedicado al desarrollo de minijuegos para dispositivos móviles. 
+    </p>
+</div>
+<hr>
+</body>
+</html>
