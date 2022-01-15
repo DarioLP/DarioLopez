03be6354eb544201e1d9dea7829b5ce4174b3e73
DarioLP <lopezdario43@gmail.com>
2022-01-15 14:00:46 -0500

    first div and resolution control

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="resolution.css">
     <title>Darío López</title>
 
     <ul class="menu">
@@ -18,21 +19,26 @@
 </head>
 <body>
 
+<!-- desktop div -->
+<div class="desktop">
 <div class="centerdiv">
 
 <div class="container">
-    <p>    Hola soy Darío López, desarrollador de software en formación.<br>
-        También soy creador de Adrem Indie Lab, un mini laboratorio dedicado al desarrollo de minijuegos para dispositivos móviles. 
+    <p>    Hola soy Darío López, desarrollador de software en formación.<br><br>
+        También soy creador de Adrem Indie Lab, un pequeño laboratorio dedicado al desarrollo de minijuegos para dispositivos móviles. 
         
     </p>
-    <div class="hrvertical"></div>  
+    <!-- <div class="hrvertical"></div>   -->
     <img id="cash" src="1.png" width="200">
       
 </div>  
 
 </div>
-
-
 <hr>
+
+</div> 
+<!-- desktop div -->
+
+
 </body>
 </html>
